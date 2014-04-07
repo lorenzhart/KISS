d7f77e9c021d21ec97c80c10e4a69b4ecb68f0e4
lonrezhart <nekogusa9@gmail.com>
2014-04-07 22:35:33 +0900

    画像の追加

---
+++ README.md
@@ -0,0 +1,8 @@
+
+
+#todo
+	 + interface
+	 	+ img
+	 	+ layout
+	 + System
+	 	+ auto scription
--- intro/introapp/models.py
+++ intro/introapp/models.py
@@ -8,12 +8,13 @@ class Post(models.Model):
     name = models.CharField(max_length=120, null=True)
     project = models.TextField(max_length=120, null=True)
     subproject = models.CharField(max_length=120, null=True)
+    subprojectDescription = models.CharField(max_length=200, null=True)
     comment = models.CharField(max_length=200, null=True)
     background = models.CharField(max_length=200, null=True)
-    subprojectDescription = models.CharField(max_length=200, null=True)
     skill = models.CharField(max_length=200, null=True)
     interest = models.CharField(max_length=200, null=True)
     introduce = models.CharField(max_length=200, null=True)
+    img = models.CharField(max_length=200, null=True)
     class Meta:
         db_table = 'persons'
 
